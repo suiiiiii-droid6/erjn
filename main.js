9ebe4858e418a1f17dfb8bf9815532efc7bb6a2e
suiiiiii-droid6 <117023976+suiiiiii-droid6@users.noreply.github.com>
2023-03-19 17:15:35 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -2,4 +2,7 @@ function changecolor(){
 document.getElementById("heading").style.color="pink";
 }
 console.log("suiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii^");
-console.log("we do not care hmmmmmmmmmmmmmmmmmmm suiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+console.log("suiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+var a = 15
+var b = 40
+console.log(a += b);
